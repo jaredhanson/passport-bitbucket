032e44c32e98dafe65bdf83c7d7dfb41c1604ad8
Samy Pessé <samypesse@gmail.com>
2013-11-14 17:07:27 +0100

    Update test for user profile with vows

--- test/index-test.js
+++ test/index-test.js
@@ -3,9 +3,7 @@ var assert = require('assert');
 var util = require('util');
 var bitbucket = require('passport-bitbucket');
 
-
 vows.describe('passport-bitbucket').addBatch({
-  
   'module': {
     'should report a version': function (x) {
       assert.isString(bitbucket.version);
