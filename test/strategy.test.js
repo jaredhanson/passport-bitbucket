e6d30b279c2e1ce1477c6e78e7303176ab6c58cc
Jared Hanson <jaredhanson@gmail.com>
2016-02-04 14:43:53 -0800

    Better error handling.

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -23,4 +23,112 @@ describe('Strategy', function() {
     });
   })
   
+  describe('error caused by invalid consumer secret sent to request token URL', function() {
+    var strategy = new BitbucketStrategy({
+      consumerKey: 'ABC123',
+      consumerSecret: 'invalid-secret'
+    }, function verify(){});
+    
+    strategy._oauth.getOAuthRequestToken = function(params, callback) {
+      callback({ statusCode: 401, data: '' });
+    }
+    
+    
+    var err;
+  
+    before(function(done) {
+      chai.passport.use(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.session = {};
+        })
+        .authenticate();
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.constructor.name).to.equal('InternalOAuthError');
+      expect(err.message).to.equal('Failed to obtain request token');
+    });
+  });
+  
+  describe('error caused by invalid request token sent to access token URL', function() {
+    var strategy = new BitbucketStrategy({
+      consumerKey: 'ABC123',
+      consumerSecret: 'invalid-secret',
+      callbackURL: 'http://www.example.test/callback'
+    }, function verify(){});
+    
+    strategy._oauth.getOAuthAccessToken = function(token, tokenSecret, verifier, callback) {
+      callback({ statusCode: 400, data: 'Invalid request token.' });
+    }
+    
+    
+    var err;
+  
+    before(function(done) {
+      chai.passport.use(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.query = {};
+          req.query['oauth_token'] = 'x-hh5s93j4hdidpola';
+          req.query['oauth_verifier'] = 'hfdp7dh39dks9884';
+          req.session = {};
+          req.session['oauth:bitbucket'] = {};
+          req.session['oauth:bitbucket']['oauth_token'] = 'x-hh5s93j4hdidpola';
+          req.session['oauth:bitbucket']['oauth_token_secret'] = 'hdhd0244k9j7ao03';
+        })
+        .authenticate();
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal("Invalid request token.");
+    });
+  });
+  
+  describe('error caused by invalid verifier sent to access token URL', function() {
+    var strategy = new BitbucketStrategy({
+      consumerKey: 'ABC123',
+      consumerSecret: 'invalid-secret',
+      callbackURL: 'http://www.example.test/callback'
+    }, function verify(){});
+    
+    strategy._oauth.getOAuthAccessToken = function(token, tokenSecret, verifier, callback) {
+      callback({ statusCode: 400, data: 'Could not verify OAuth request.' });
+    }
+    
+    
+    var err;
+  
+    before(function(done) {
+      chai.passport.use(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.query = {};
+          req.query['oauth_token'] = 'x-hh5s93j4hdidpola';
+          req.query['oauth_verifier'] = 'hfdp7dh39dks9884';
+          req.session = {};
+          req.session['oauth:bitbucket'] = {};
+          req.session['oauth:bitbucket']['oauth_token'] = 'x-hh5s93j4hdidpola';
+          req.session['oauth:bitbucket']['oauth_token_secret'] = 'hdhd0244k9j7ao03';
+        })
+        .authenticate();
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal("Could not verify OAuth request.");
+    });
+  });
+  
 });
